790afb382a114431c1d27f9cc7ce70727f151430
chakku <chakkurai0724@outlook.jp>
2018-11-02 16:29:45 +0900

    pwa test

--- OCW/static/js/serviceworker.js
+++ OCW/static/js/serviceworker.js
@@ -5,7 +5,8 @@ self.addEventListener('install', function (event) {
         caches.open(staticCacheName).then(function (cache) {
             console.log("FINDING1");
             return cache.addAll([
-                '/base_layout'
+                //'/base_layout'
+                '/'
             ]);
         })
     );
@@ -15,11 +16,14 @@ self.addEventListener('fetch', function (event) {
     var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
         if ((requestUrl.pathname === '/')) {
+            console.log("Pass1");
             console.log("FINDING2");
-            event.respondWith(caches.match('/base_layout'));
+            //event.respondWith(caches.match('/base_layout'));
+            event.respondWith(caches.match('/'));
             return;
         }
     }
+    console.log("Pass2");
     event.respondWith(
         caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
--- OCW/urls.py
+++ OCW/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
+# from django.conf.urls import url
 from . import views
 
 app_name = 'app'
 urlpatterns = [
-    #url(r'^$', views.test_response),
-    url('^$', views.toppage),
-    url('abc/', views.search_and_result),
-	url('lecture/',views.lecture,name='lecture'),
-	url('department/',views.department_page,name='department'),
-    url('base_layout/',views.base_layout,name='base_layout'),
+    path(r'', views.toppage),
+    path(r'abc', views.search_and_result),
+    path(r'lecture', views.lecture, name='lecture'),
+    path(r'department', views.department_page, name='department'),
+    #path(r'base_layout', views.base_layout, name='base_layout'),
 ]
