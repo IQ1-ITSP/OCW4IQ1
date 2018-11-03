1a54944f8e078c96e3d6750e60748996b8d1fce0
hi-watana <hero95alive@gmail.com>
2018-11-03 10:12:46 +0900

    refs #16: Separated JS from templates/department.html.

---
+++ OCW/static/js/department.js
@@ -0,0 +1,32 @@
+function changeVisibility(lecture_tuple) {
+    if (lecture_tuple.getAttribute('data-series') === 'true' && lecture_tuple.getAttribute('data-opening_department') === 'true')
+        lecture_tuple.setAttribute("style", '');
+    else
+        lecture_tuple.setAttribute("style", 'display: none;');
+}
+
+function checkboxListener(_id, data_type) {
+    var rows = document.getElementsByClassName(_id);
+
+    var value = (document.getElementById(_id).checked) ? 'true' : 'false';
+    for (var row of rows) {
+        row.setAttribute(data_type, value);
+        changeVisibility(row);
+    }
+}
+
+function changeAllCheckboxes(className, bool_value) {
+    var checkboxes = document.getElementsByName(className);
+
+    for (var checkbox of checkboxes) {
+        checkbox.checked = bool_value;
+    }
+    var rows = document.getElementById('tbody-lecture').getElementsByTagName('tr');
+
+    var data_value = (bool_value) ? 'true' : 'false';
+    var data_type = 'data-' + className;
+    for (var row of rows) {
+        row.setAttribute(data_type, data_value);
+        changeVisibility(row);
+    }
+}
